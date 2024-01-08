5743b916d6cc2c5a13f0d7e7b1904103cf80abff
sri2440 <131736323+sri2440@users.noreply.github.com>
2024-01-08 14:50:07 +0530

    Add files via upload

---
+++ sample.py
@@ -0,0 +1,5 @@
+# List comprehension to generate squares of even numbers from 1 to 10
+squares_of_even_numbers = [x**2 for x in range(1, 11) if x % 2 == 0]
+
+# Print the result
+print(squares_of_even_numbers)
